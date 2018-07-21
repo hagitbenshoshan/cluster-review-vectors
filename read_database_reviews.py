73db4ba363b6ee8ef927db0bb2cd5656312c64b0
hagitbenshoshan <hagit@cooladata.com>
2018-07-21 21:45:20 +0300

    Update read_database_reviews.py

--- read_database_reviews.py
+++ read_database_reviews.py
@@ -5,18 +5,9 @@ Created on Sat Jul 21 10:22:28 2018
 @author: user
 
 
-pip install --upgrade google-cloud-bigquery
-created json file for service account https://cloud.google.com/bigquery/docs/reference/libraries#client-libraries-install-python
-SA name : hagit-926@cooladata-cs-solutions.iam.gserviceaccount.com
-created json file in C:\Users\user\Documents\Python Scripts\thesis\cooladata-cs-solutions-3be46bd05169.json
-
 set env variable GOOGLE_APPLICATION_CREDENTIALS=C:\Users\user\Documents\Python Scripts\thesis\cooladata-cs-solutions-3be46bd05169.json 
 
-import pandas as pd
-# https://github.com/SohierDane/BigQuery_Helper
-from bq_helper import BigQueryHelper
 
-bq_assistant = BigQueryHelper("bigquery-public-data", "github_repos")
 
 """
 # Imports the Google Cloud client library
@@ -25,7 +16,7 @@ import pandas as pd
  
 # Instantiates a client
 bigquery_client = bigquery.Client()
-projectid = "cooladata-cs-solutions"
+projectid = "thesis-solutions"
 
 for j in range(10):
     print (j) 
